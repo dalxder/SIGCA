a6ea676c09bb2a2e2653a4785883ac2df9fd3d5c
Dalxder <dalxder@gmail.com>
2018-07-15 16:41:26 -0500

    Update appLimno.js

--- dashboard/lib/appLimno.js
+++ dashboard/lib/appLimno.js
@@ -120,7 +120,7 @@ dataTable = dc.dataTable('#data-table',groupname);
      });
 
   dc.renderAll(groupname);  
-  mymap.map().setView([4.4,-74.5], 8);
+  //mymap.map().setView([4.4,-74.5], 8);
 
   myLayout.on('stateChanged', function() {   
       mymap.map().invalidateSize();  
